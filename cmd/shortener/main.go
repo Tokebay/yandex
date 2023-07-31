3a820ef7403d439d454b14c213089087661a8d9f
Ибрагимов Токен <00025581@nb>
2023-07-31 18:15:14 +0600

    iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,41 @@
 package main
 
-func main() {}
+import (
+	"math/rand"
+	"net/http"
+	"time"
+
+	bs "github.com/catinello/base62"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getReqHandler)
+
+	err := http.ListenAndServe(":8080", mux)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getReqHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(307)
+		w.Header().Set("Location", "https://practicum.yandex.ru/")
+
+	case "POST":
+		r.Header.Add("Content-type", "text/plain")
+		w.WriteHeader(201)
+		w.Header().Set("Content-type", "text/plain")
+
+		rand.Seed(time.Now().UnixNano())
+
+		encode := (bs.Encode(rand.Intn(9999999999)))
+		w.Write([]byte("http://localhost:8080/" + encode))
+	default:
+		w.WriteHeader(400)
+
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Tokebay/yandex
+
+go 1.19
+
+require github.com/catinello/base62 v0.0.0-20210103152244-29b605f01e9b // indirect
