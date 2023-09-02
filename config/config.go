73b3db6f8337143f96fb5974d683c6ef8df7857b
Ибрагимов Токен <00025581@nb>
2023-09-02 09:55:49 +0600

    change port

--- config/config.go
+++ config/config.go
@@ -18,9 +18,9 @@ func NewConfig() *Config {
 	config := &Config{}
 	config.parseEnv()
 
-	flag.StringVar(&config.ServerAddress, "a", "localhost:8080", "HTTP server address")
-	flag.StringVar(&config.BaseURL, "b", "http://localhost:8080", "Base URL for shortened URLs")
-	flag.IntVar(&config.ServerPort, "p", 8080, "HTTP server port")
+	flag.StringVar(&config.ServerAddress, "a", "localhost:8888", "HTTP server address")
+	flag.StringVar(&config.BaseURL, "b", "http://localhost:8888", "Base URL for shortened URLs")
+	flag.IntVar(&config.ServerPort, "p", 8888, "HTTP server port")
 	flag.StringVar(&config.FileStoragePath, "f", "/tmp/short-url-db.json", "Path to FILE_STORAGE_PATH")
 
 	flag.Parse()
